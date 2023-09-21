dbad9553b589820d92ea0e64c991effe30db7a52
luansilva25 <luansilva250807@gmail.com>
2023-09-21 11:33:14 -0300

    a

--- js.js
+++ js.js
@@ -1,27 +1,32 @@
 
 
-function calcular(){
-const peso= document.getElementById('peso').value;
-const altura= document.getElementById('altura').value;
-const imc= peso/altura**2
+function calcular() {
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const imc = peso / altura ** 2
 
-if(imc <= 17){
-    alert('muito abaixo do peso')
+    if (imc <= 17) {
+        alert('muito abaixo do peso')
+    }
+    else if (imc > 17 && imc <= 18.5) {
+        alert('abaixo do peso')
+    }
+    else if (imc > 18.5 && imc <= 25) {
+        alert('peso normal')
+    }
+    else if (imc > 25 && imc <= 30) {
+        alert('obesidade')
+    }
+    else if (imc > 30 && imc <= 35) {
+        alert('obesidade severa')
+    }
+    else {
+        alert('obesidade morbita')
+    }
+    alert(` seu IMC é de: ${imc.toFixed(1)}`)
 }
-else if(imc > 17 && imc <= 18.5){
-    alert('abaixo do peso')
-}
-else if(imc > 18.5 && imc <= 25 ){
-    alert('peso normal')
-}
-else if(imc > 25 && imc <= 30){
-    alert('obesidade')
-}
-else if(imc > 30 && imc <= 35){
-    alert('obesidade severa')
-}
-else{
-    alert('obesidade morbita')
-}
-alert(` seu IMC é de: ${imc.toFixed(1)}`)
+
+function reset() {
+    document.getElementById('peso').value = "";
+    document.getElementById('altura').value='';
 }
