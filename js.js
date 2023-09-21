257e34d89e21b9ddd53ab7791390524eecdc4ae9
luansilva25 <luansilva250807@gmail.com>
2023-09-21 10:57:10 -0300

    a

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
 
+
+function calcular(){
+const peso= document.getElementById('peso').value;
+const altura= document.getElementById('altura').value;
+const imc= peso/altura**2
+
+if(imc <= 17){
+    alert('muito abaixo do peso')
+}
+else if(imc > 17 && imc <= 18.5){
+    alert('abaixo do peso')
+}
+else if(imc > 18.5 && imc <= 25 ){
+    alert('peso normal')
+}
+else if(imc > 25 && imc <= 30){
+    alert('obesidade')
+}
+else if(imc > 30 && imc <= 35){
+    alert('obesidade severa')
+}
+else{
+    alert('obesidade morbita')
+}
+alert(`${imc.toFixed(2)}`)
+}
